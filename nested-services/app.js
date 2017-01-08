f72705a065821ccc1ad939cc3737ff3806b7240b
nswbmw <gxqzk@126.com>
2017-01-08 14:05:53 +0800

    add: nested-services example

---
+++ nested-services/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Paloma = require('paloma');
+const app = new Paloma();
+const usage = require('../usage');
+
+app.controller('indexCtrl', function (ctx, next, indexService) {
+  ctx.body = `Hello, ${indexService.User.getName()}`;
+});
+
+app.service('indexService.User', function () {
+  this.getName = function () {
+    return 'Paloma';
+  };
+});
+
+app.route({
+  method: 'GET',
+  path: '/',
+  controller: 'indexCtrl'
+});
+
+app.listen(3000, () => {
+  usage('curl http://localhost:3000', 'Hello, Paloma');
+});
